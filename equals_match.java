2a423b94bccbc0425ac687fbb2d0517bc374d71a
Harsha Prasad K M <harshaprasad012@gmail.com>
2026-02-24 22:08:45 +0530

    Add files via upload

---
+++ equals_match.java
@@ -0,0 +1,15 @@
+class equals_match
+{
+public static void main(String args[])
+
+{
+		String s1="Hello";
+		String s2= new String(s1);
+		System.out.println(s1.equals(s2));
+		System.out.println((s1==s2));
+
+}
+
+
+}
+
